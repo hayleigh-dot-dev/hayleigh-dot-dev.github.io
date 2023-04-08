6177ed39a2ee9d5f6eca6db0837aac4e52651e45
Hayleigh Thompson <me@hayleigh.dev>
2023-04-08 22:57:00 +0100

    :heavy_plus_sign: Add mdx dependency and integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), mdx()],
 });
