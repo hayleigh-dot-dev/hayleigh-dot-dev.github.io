be8cf1273ee8baa374a57811d91ba2e00c5ba435
Hayleigh Thompson <me@hayleigh.dev>
2023-04-17 01:50:16 +0100

    :poop: Add a bunch of things. Write some words.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
+import { readFileSync } from "fs";
+
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+const gleamGrammar = readFileSync("./assets/gleam.tmLanguage.json", "utf8");
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "vitesse-dark",
+      langs: [{ id: "gleam", scopeName: "source.gleam", grammar: JSON.parse(gleamGrammar) }],
+    },
+  },
 });
